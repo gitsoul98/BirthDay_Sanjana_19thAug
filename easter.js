0d93dd16171dfde9cfde6489f4d9d3ceb1fa8d87
روح <almightyultimatesoul@gmail.com>
2026-06-29 14:31:35 +0530

    Create easter.js

---
+++ easter.js
@@ -0,0 +1,23 @@
+window.sj = () => {
+    console.clear();
+
+    console.log("%c🎉 You actually found it!", "color:#ff69b4;font-size:24px;font-weight:bold;");
+    console.log("");
+
+    console.log("Wow...");
+    console.log("You must have a pretty good idea of who made this by now. 👀");
+    console.log("");
+
+    console.log("There's no need to say the name.");
+    console.log("If you know, you know. 🙂");
+    console.log("");
+
+    console.log("%cOne last thing...",
+        "color:#ffd166;font-size:18px;font-weight:bold;");
+
+    console.log("Thanks for missing the class. 😂");
+    console.log("");
+
+    console.log("And...");
+    console.log("Happy Birthday once again! 🎂✨");
+};
